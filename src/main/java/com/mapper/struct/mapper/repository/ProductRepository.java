c66d10159be2d9103b9c9ea6d737129230b39317
Maksuedson <maksuedson@hotmail.com>
2024-08-24 16:12:24 -0300

    Created ProductService

--- src/main/java/com/mapper/struct/mapper/repository/ProductRepository.java
+++ src/main/java/com/mapper/struct/mapper/repository/ProductRepository.java
@@ -1,4 +1,4 @@
-package com.mapper.struct.mapper;
+package com.mapper.struct.mapper.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
---
+++ src/main/java/com/mapper/struct/mapper/services/ProductService.java
@@ -0,0 +1,38 @@
+package com.mapper.struct.mapper.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mapper.struct.mapper.dto.ProductDto;
+import com.mapper.struct.mapper.model.Product;
+import com.mapper.struct.mapper.repository.ProductRepository;
+
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public ProductDto convertToDto(Product product) {
+		ProductDto productDto = new ProductDto();
+		productDto.setId(product.getId());
+		productDto.setNome(product.getName());
+		productDto.setPreco(product.getPrice());
+		
+		return productDto;
+	}
+	
+	public Product convertToEntity(ProductDto dto) {
+		Product product = new Product();
+		product.setId(dto.getId());
+		product.setName(dto.getNome());
+		product.setPrice(dto.getPreco());
+		
+		return product;
+	}
+	
+	public ProductDto saveProduct(ProductDto productDto) {
+		Product product = convertToEntity(productDto);
+		Product saveProduct = repo.save(product);
+		
+		return convertToDto(saveProduct);
+	}
+}
