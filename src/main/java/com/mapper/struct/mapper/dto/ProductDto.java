9ce07dfebb2118f5c082ed396e2039ecb6b4ba82
Maksuedson <maksuedson@hotmail.com>
2024-08-24 16:12:00 -0300

    Added NoArgsConstructor anotation

--- src/main/java/com/mapper/struct/mapper/dto/ProductDto.java
+++ src/main/java/com/mapper/struct/mapper/dto/ProductDto.java
@@ -2,11 +2,13 @@ package com.mapper.struct.mapper.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductDto {
 
 	private Long id;
